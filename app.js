de9bad18db34409f20bb0fad922be123d57b4c43
MadhushaPrasad <madushaprasad21@gmail.com>
2023-03-25 14:31:50 +0530

    node js file writing

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 // console.log('hello world');
 
-// read file
+
 
 const fs = require('fs');
+// read file
+// fs.readFile('file.txt', 'utf-8', function (err, data) {
+//   if (err) throw err;
+//   console.log(data);
+// });
 
-fs.readFile('file.txt', 'utf-8', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
+//file writing
+fs.writeFile(
+    "file.text",
+    "This is testing of file writing",
+    function(error,data){
+        if(error) throw error;
+        console.log("File written successfully");
+    }
+);

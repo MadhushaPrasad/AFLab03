0614988e43fe1fd293202c18ad634623fb3183e2
MadhushaPrasad <madushaprasad21@gmail.com>
2023-03-25 15:37:11 +0530

    Promises

--- app.js
+++ app.js
@@ -44,5 +44,21 @@ const fs = require('fs');
 //   });
 
 //Using a module
-const myModule = require('./my-module.js');
-console.log(myModule.myFunction());
+// const myModule = require('./my-module.js');
+// console.log(myModule.myFunction());
+
+//Promises
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve('Success!');
+  } else {
+    reject('Failure!');
+  }
+});
+myPromise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });

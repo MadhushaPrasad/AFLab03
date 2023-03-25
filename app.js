fede2f25ec29a337c9245499e4b9cb54c9c0834b
MadhushaPrasad <madushaprasad21@gmail.com>
2023-03-25 15:39:01 +0530

    Async/Await

--- app.js
+++ app.js
@@ -63,13 +63,13 @@ const fs = require('fs');
 //     console.log(error);
 //   });
 
-//Async/Await
-async function myFunction() {
-  try {
-    const result = await myPromise;
-    console.log(result);
-  } catch (error) {
-    console.log(error);
-  }
-}
-myFunction();
+// //Async/Await
+// async function myFunction() {
+//   try {
+//     const result = await myPromise;
+//     console.log(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// myFunction();
